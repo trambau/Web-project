f8a012cdf156196f4e394d61c4332e3abcaece0b
trambau <ramelapierre@gmail.com>
2020-10-10 14:22:43 +0200

    1

--- test.php
+++ test.php
@@ -1,1 +1,17 @@
-
+<html>
+<title>
+</title>
+<head>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="get">
+    email:
+    <input type="text">
+    pswd:
+    <input type="text">
+    #phone
+    <input type="text">
+    <input type="submit" value="Create">
+    </form>
+</body>
+</html>
